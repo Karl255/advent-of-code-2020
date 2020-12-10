2db1474fef9f774a2a51a9198114a5c0da473434
Karl255 <49595438+Karl255@users.noreply.github.com>
2020-12-10 08:58:49 +0100

    Added day 10

---
+++ AoC2020-10-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+var input = System.IO.File
+	.ReadAllLines("input.txt")
+	.Select(int.Parse);
+
+int[] lines = input
+	.OrderBy(x => x)
+	.Append(input.Max() + 3)
+	.ToArray();
+
+int previous = 0;
+int diff1Count = 0;
+int diff3Count = 0;
+
+foreach (int num in lines)
+{
+	long diff = num - previous;
+
+	switch (diff)
+	{
+		case 1:
+			diff1Count++;
+			break;
+		case 3:
+			diff3Count++;
+			break;
+		default:
+			throw new Exception();
+	};
+
+	previous = num;
+}
+
+Console.WriteLine(diff1Count * diff3Count);
---
+++ AoC2020-10-2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+var input = System.IO.File.ReadAllLines("input.txt").Select(int.Parse);
+
+Stopwatch sw = new();
+sw.Start();
+
+int[] lines = input
+	.OrderBy(x => x)
+	.Prepend(0)
+	.Append(input.Max() + 3)
+	.ToArray();
+
+Dictionary<int, long> cache = new();
+
+long result = Arrangements(lines) + 1;
+sw.Stop();
+
+Console.WriteLine(result);
+Console.WriteLine(sw.Elapsed);
+
+long Arrangements(int[] input, int startIndex = 0)
+{
+	long count = 0;
+
+	for (int i = startIndex; i < input.Length - 3; i++)
+	{
+		// detect if there is a branching in the tree
+		if (input[i + 2] - input[i] <= 3)
+		{
+			if (cache.ContainsKey(input[i]))
+				count += cache[input[i]];
+			else
+			{
+				count += Arrangements(input, i + 1);
+				count += 1 + Arrangements(input, i + 2);
+
+				// if tripple branch, add the 3rd branch
+				if (input[i + 3] - input[i] <= 3)
+					count += 1 + Arrangements(input, i + 3);
+
+				cache.Add(input[i], count);
+			}
+
+			break;
+		}
+	}
+
+	return count;
+}
