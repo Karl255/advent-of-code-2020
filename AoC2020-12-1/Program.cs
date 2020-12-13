9c8a555611dbbd61e2dfaa5f0e2114b5376ed1e5
Karl255 <49595438+Karl255@users.noreply.github.com>
2020-12-13 07:36:51 +0100

    Added day 12

---
+++ AoC2020-12-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+var input = System.IO.File.ReadAllLines("input.txt")
+	.Select(x => (instruction: x[0], value: int.Parse(x[1..])))
+	.ToArray();
+
+(int x, int y) pos = (0, 0);
+int rotation = 0;
+
+foreach (var line in input)
+{
+	switch (line.instruction)
+	{
+		case 'N':
+			pos.y += line.value;
+			break;
+		case 'S':
+			pos.y -= line.value;
+			break;
+		case 'E':
+			pos.x += line.value;
+			break;
+		case 'W':
+			pos.x -= line.value;
+			break;
+		case 'L':
+			rotation++;
+			rotation %= 4;
+			break;
+		case 'R':
+			rotation += 3;
+			rotation %= 4;
+			break;
+		case 'F':
+			_ = rotation switch
+			{
+				0 => pos.x += line.value,
+				1 => pos.y += line.value,
+				2 => pos.x -= line.value,
+				3 => pos.y -= line.value,
+				_ => throw new Exception()
+			};
+			break;
+		default:
+			break;
+	};
+}
+
+Console.WriteLine(Math.Abs(pos.x) + Math.Abs(pos.y));
---
+++ AoC2020-12-2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+var input = System.IO.File.ReadAllLines("input.txt")
+	.Select(x => (instruction: x[0], value: int.Parse(x[1..])))
+	.ToArray();
+
+(int x, int y) pos = (0, 0);
+(int x, int y) waypoint = (10, 1);
+
+foreach (var line in input)
+{
+	switch (line.instruction)
+	{
+		case 'N':
+			waypoint.y += line.value;
+			break;
+		case 'S':
+			waypoint.y -= line.value;
+			break;
+		case 'E':
+			waypoint.x += line.value;
+			break;
+		case 'W':
+			waypoint.x -= line.value;
+			break;
+		case 'L':
+			for (int i = 0; i < line.value / 90; i++)
+				waypoint = (-waypoint.y, waypoint.x);
+			break;
+		case 'R':
+			for (int i = 0; i < line.value / 90; i++)
+				waypoint = (waypoint.y, -waypoint.x);
+			break;
+		case 'F':
+			pos.x += waypoint.x * line.value;
+			pos.y += waypoint.y * line.value;
+			break;
+		default:
+			break;
+	};
+}
+
+Console.WriteLine(Math.Abs(pos.x) + Math.Abs(pos.y));
