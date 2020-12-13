9c94c6bc77ba5c3e2bb02558749e54b167612d5e
Karl255 <49595438+Karl255@users.noreply.github.com>
2020-12-13 11:08:42 +0100

    Added day 13 (part 1 only)

---
+++ AoC2020-13-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+string[] input = System.IO.File.ReadAllLines("input.txt");
+int startTime = int.Parse(input[0]);
+var lines = input[1]
+	.Split(',')
+	.Where(x => x != "x")
+	.Select(int.Parse)
+	.Select(x => (waitTime: (int)Math.Ceiling(startTime / (double)x) * x - startTime, id: x));
+
+var minLine = lines.First(x => x.waitTime == lines.Min(x => x.waitTime));
+
+Console.WriteLine(minLine.id * minLine.waitTime);
