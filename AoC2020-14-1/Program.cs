f708c891bb4aa0f5a42423e01fbc6e45516c6cb6
Karl255 <49595438+Karl255@users.noreply.github.com>
2020-12-14 10:41:05 +0100

    Added day 14

---
+++ AoC2020-14-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+string[] input = System.IO.File.ReadAllLines("input.txt");
+
+long bitMask = ((long)1 << 36) - 1;
+long maskValue = 0;
+Dictionary<int, long> memory = new();
+
+for (int i = 0; i < input.Length; i++)
+{
+	string[] t = input[i].Split(" = ");
+
+	if (input[i][1] == 'a')
+	{
+		string maskStrValue = t[1];
+
+		bitMask = Convert.ToInt64(maskStrValue
+			.Replace('1', '0')
+			.Replace('X', '1'),
+			2);
+
+		maskValue = Convert.ToInt64(maskStrValue.Replace('X', '0'), 2);
+	}
+	else
+	{
+		int address = int.Parse(t[0][4..^1]);
+		long value = long.Parse(t[1]);
+
+		memory[address] = value & bitMask | maskValue;
+	}
+}
+
+Console.WriteLine(memory.Sum(x => x.Value));
---
+++ AoC2020-14-2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+string[] input = System.IO.File.ReadAllLines("input.txt");
+
+string mask = "";
+Dictionary<long, long> memory = new();
+
+for (int i = 0; i < input.Length; i++)
+{
+	string[] t = input[i].Split(" = ");
+
+	if (t[0][1] == 'a')
+		mask = t[1];
+	else
+	{
+		long address = int.Parse(t[0][4..^1]);
+		long value = long.Parse(t[1]);
+
+		for (int j = 0; j < 36; j++)
+			if (mask[j] == '1')
+				address = address | (1L << 35 - j);
+
+		AllCombos(mask.Count(c => c == 'X'), list =>
+		{
+			int k = 0;
+			for (int j = 0; j < 36; j++)
+				if (mask[j] == 'X')
+				{
+					if (list[k] == 1)
+						address = address | (1L << 35 - j);
+					else
+						address = address & ~(1L << 35 - j);
+					k++;
+				}
+
+			memory[address] = value;
+		});
+	}
+}
+
+Console.WriteLine(memory.Sum(x => x.Value));
+
+void AllCombos(int n, Action<int[]> callback)
+{
+	int[] list = new int[n];
+
+	void allCombos(int[] list, int i)
+	{
+		if (i == list.Length)
+			callback(list);
+		else
+		{
+			list[i] = 0;
+			allCombos(list, i + 1);
+			list[i] = 1;
+			allCombos(list, i + 1);
+		}
+	}
+
+	allCombos(list, 0);
+}
