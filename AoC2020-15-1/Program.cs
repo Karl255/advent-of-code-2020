dd7b273b281854a32e0b5bd24c95524a9c5b926c
Karl255 <49595438+Karl255@users.noreply.github.com>
2020-12-15 07:31:21 +0100

    Added day 15

---
+++ AoC2020-15-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<int> numbers = System.IO.File.ReadAllText("input.txt")
+	.Split(',')
+	.Select(int.Parse)
+	.ToList();
+
+numbers.Capacity = 2020;
+
+for (int current = numbers.Count - 1; current < 2020 - 1; current++)
+{
+	if (numbers.Count(n => n == numbers[current]) == 1)
+		numbers.Add(0);
+	else
+	{
+		int indexOfLast = numbers.LastIndexOf(numbers[current], numbers.Count - 2);
+		numbers.Add(current - indexOfLast);
+	}
+}
+
+Console.WriteLine(numbers.Last());
---
+++ AoC2020-15-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int[] startNumbers = System.IO.File.ReadAllText("input.txt")
+	.Split(',')
+	.Select(int.Parse)
+	.ToArray();
+
+Dictionary<int, int> numbersLastTurn = startNumbers[0..^1]
+	.Select((x, i) => (key: x, value: i + 1))
+	.ToDictionary(x => x.key, x => x.value);
+
+int nextNumber = startNumbers[^1];
+
+for (int i = numbersLastTurn.Count + 1; i < 30000000; i++)
+{
+	if (numbersLastTurn.ContainsKey(nextNumber))
+	{
+		int t = numbersLastTurn[nextNumber];
+		numbersLastTurn[nextNumber] = i;
+		nextNumber = i - t;
+	}
+	else
+	{
+		numbersLastTurn.Add(nextNumber, i);
+		nextNumber = 0;
+	}
+}
+
+Console.WriteLine(nextNumber);
