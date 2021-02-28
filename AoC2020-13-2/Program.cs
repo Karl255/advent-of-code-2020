0e5ca4dd88de907ca94f99fe1e5e84efc8968501
Karl255 <49595438+Karl255@users.noreply.github.com>
2021-03-01 00:14:35 +0100

    Added day 13 part 2

---
+++ AoC2020-13-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+string[] input = System.IO.File.ReadAllLines("input.txt");
+var lines = input[1]
+	.Split(',')
+	.Select((id, index) => (id: id, offset: index))
+	.Where(line => line.id != "x")
+	.Select(line => new BusLine(long.Parse(line.id), (long)line.offset))
+	.ToImmutableArray();
+
+long time = lines[0].Id;
+long period = lines[0].Id;
+
+foreach (var line in lines.Skip(1))
+{
+	while ((time + line.Offset) % line.Id != 0)
+		time += period;
+
+	period *= line.Id;
+}
+
+Console.WriteLine(time);
+
+record BusLine(long Id, long Offset);
